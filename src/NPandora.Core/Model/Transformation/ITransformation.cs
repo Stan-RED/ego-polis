e71ecb27e8fc148b9cdb79d30370c99a35f17509
stan <stanislav@yarmonov.pro>
2017-09-05 16:19:07 +0300

    +Added transformation idea.

---
+++ src/NPandora.Core/Model/Transformation/ITransformation.cs
@@ -0,0 +1,17 @@
+﻿using NPandora.Feature;
+
+namespace NPandora.Model.Transformation
+{
+    /// <summary>
+    /// TODO:Responsible for defining transformation contract bwetween
+    /// <typeparamref name="TRequest">request</typeparamref> and
+    /// <typeparamref name="TResponse">response</typeparamref>.
+    /// </summary>
+    /// <typeparam name="TRequest"></typeparam>
+    /// <typeparam name="TResponse"></typeparam>
+    public interface ITransformation<TRequest, TResponse>
+        : IFeature<TRequest, TResponse>
+    {
+
+    }
+}
