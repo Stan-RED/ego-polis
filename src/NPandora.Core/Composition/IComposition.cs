30a965fd8817a97fa664e74ef95b872bc064033b
stan <stanislav@yarmonov.pro>
2017-09-05 16:16:13 +0300

    +Added composition idea.

---
+++ src/NPandora.Core/Composition/IComposition.cs
@@ -0,0 +1,10 @@
+﻿namespace NPandora.Composition
+{
+    /// <summary>
+    /// TODO:Application's building block.
+    /// </summary>
+    public interface IComposition
+    {
+        void Compose(ICompositionContext contex);
+    }
+}
---
+++ src/NPandora.Core/Composition/ICompositionContext.cs
@@ -0,0 +1,10 @@
+﻿namespace NPandora.Composition
+{
+    /// <summary>
+    /// TODO:Supports application's "vertical".
+    /// </summary>
+    public interface ICompositionContext
+    {
+
+    }
+}
