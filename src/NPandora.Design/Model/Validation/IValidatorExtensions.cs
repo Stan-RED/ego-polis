367dfc29b89dc76a6191158fdcf86872fe77c3c3
stan <stanislav@yarmonov.pro>
2017-09-06 10:12:08 +0300

    +Added IValidator idea.

---
+++ src/NPandora.Design/Model/Validation/IValidatorExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace NPandora.Model.Validation
+{
+    /// <summary>
+    /// Useful method extensions for <see cref="IValidator{TModel}"/>.
+    /// </summary>
+    public static class IValidatorExtensions
+    {
+        /// <summary>
+        /// TODO:Base <see cref="IValidator{TModel}.Validate(TModel)"/> methods does not
+        /// throw exception, so with this extension we can ensure.
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <param name="validator"></param>
+        /// <param name="model"></param>
+        public static void Ensure<TModel>(this IValidator<TModel> validator, TModel model)
+        {
+            var exception = validator.Validate(model);
+
+            if (exception != null)
+            {
+                throw exception;
+            }
+        }
+    }
+}
---
+++ src/NPandora.Design/Quality/TestException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NPandora.Quality
+{
+    /// <summary>
+    /// TODO:During testing we frequently need some exception that is not expected
+    /// to be thrown by application.
+    /// </summary>
+    public class TestException : Exception
+    {
+
+    }
+}
