70ac1cc0ccd6185898799f09e6ed8334d0d2c267
stan <stanislav@yarmonov.pro>
2018-08-01 20:15:46 +0300

    +Added IEnvironmentVariables idea.

---
+++ src/Codesophy.Design/Environment/IEnvironmentVariables.cs
@@ -0,0 +1,13 @@
+using Codesophy.Configuration;
+
+namespace Codesophy.Environment
+{
+    /// <todo>
+    /// What is a better way to organize access to environment variables?
+    /// Use such interface o share with <see cref="ISetting{TValue}"/>.
+    /// </todo>
+    public interface IEnvironmentVariables
+    {
+        
+    }
+}
