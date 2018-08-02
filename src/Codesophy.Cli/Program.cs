1f82c60375c37cab9255f0c37f5f93861956368a
stan <stanislav@yarmonov.pro>
2018-08-02 09:03:32 +0300

    +Added CLI boilerplate.

---
+++ src/Codesophy.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace Codesophy.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO:
+            var name = Assembly.GetEntryAssembly().GetName();
+            Console.WriteLine($"Codesophy CLI {name.Version}");
+        }
+    }
+}
