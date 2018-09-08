91131ded7b99873239a0083500c5d9fee128d445
yavulan <yavulan@gmail.com>
2018-09-08 14:51:46 +0300

    +Added custom scrollbars.

---
+++ src/Codesophy.Ide.Server/ClientApp/src/app/_core/services/scrollbar.service.ts
@@ -0,0 +1,60 @@
+import PerfectScrollbar from "perfect-scrollbar";
+import { Inject, Injectable, OnDestroy } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { NavigationEnd, Router } from "@angular/router";
+import { Subscription } from "rxjs";
+import { filter } from "rxjs/operators";
+
+/**
+ * Service for 3d-party https://github.com/utatti/perfect-scrollbar
+ */
+@Injectable()
+export class ScrollbarService implements OnDestroy {
+  options: PerfectScrollbar.Options = {
+    wheelSpeed: 2,
+    wheelPropagation: false
+  };
+
+  scrollbars: Array<PerfectScrollbar> = [];
+  subscription: Subscription;
+
+  constructor(@Inject(DOCUMENT) private document: Document, private router: Router) {
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  init() {
+    // Destroys all existing scrollbars.
+    this.scrollbars.forEach(scrollbar => scrollbar.destroy());
+    this.scrollbars = [];
+
+    // Creates scrollbars for each scrollbar container.
+    this.getScrollBarContainers()
+      .forEach(scrollbar => this.scrollbars.push(new PerfectScrollbar(scrollbar, this.options)));
+
+    // Updates existing scrollbars on NavigationEnd.
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => this.update());
+  }
+
+  update() {
+    // Scrolls to top.
+    this.getScrollBarContainers().forEach(scrollbar => scrollbar.scrollTop = 0);
+
+    // Updates existing scrollbars.
+    setTimeout(() => {
+      this.scrollbars.forEach(scrollbar => scrollbar.update());
+    });
+  }
+
+  /**
+   * Returns containers that need custom scrollbars.
+   * @returns {Array<HTMLElement>} Array containing scrollbar containers.
+   */
+  private getScrollBarContainers(): Array<HTMLElement> {
+    return Array.from(this.document.querySelectorAll(".app-scrollbar-container"));
+  }
+}
--- src/Codesophy.Ide.Server/ClientApp/src/app/app.component.ts
+++ src/Codesophy.Ide.Server/ClientApp/src/app/app.component.ts
@@ -1,18 +1,26 @@
-import { Component, HostBinding } from "@angular/core";
+import { AfterViewInit, Component, HostBinding, OnInit } from "@angular/core";
 
-import { ThemeService } from "./_core/services";
+import { ScrollbarService, ThemeService } from "./_core/services";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   @HostBinding("class") get themeCssClass() {
     return this.theme.themeCssClass;
   }
 
-  constructor(public theme: ThemeService) {
+  constructor(public theme: ThemeService, private scrollbar: ScrollbarService) {
+  }
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    this.scrollbar.init();
   }
 
   log(msg: string) {
