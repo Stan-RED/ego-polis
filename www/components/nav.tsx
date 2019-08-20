1dbfd6c5e8b42b67176c4dc1e83c82b844265108
stan <stanislav@yarmonov.pro>
2019-08-20 21:11:23 +0300

    *Typescript support for www.

--- www/components/nav.tsx
+++ www/components/nav.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react"
+import Link from "next/link"
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
+  { href: "https://zeit.co/now", label: "ZEIT", key: "" },
+  { href: "https://github.com/zeit/next.js", label: "GitHub", key: "" }
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`
   return link
@@ -13,7 +13,7 @@ const Nav = () => (
   <nav>
     <ul>
       <li>
-        <Link href='/'>
+        <Link href="/">
           <a>Home</a>
         </Link>
       </li>
