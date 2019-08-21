a8c4263c95e82a92c1672294db27fb9141201b80
stan <stanislav@yarmonov.pro>
2019-08-21 20:11:14 +0300

    +Katex support added for MDX.

--- www/next.config.js
+++ www/next.config.js
@@ -1,14 +1,30 @@
+const withPlugins = require('next-compose-plugins');
+
 const images = require("remark-images")
 const emoji = require("remark-emoji")
 const math = require("remark-math")
+const katex = require("rehype-katex")
 
 const withMDX = require("@next/mdx")({
-    extension: /\.mdx?$/,
-    options: {
-        remarkPlugins: [ images, emoji, math ]
-    }
+	extension: /\.(md|mdx)$/,
+	options: {
+		remarkPlugins: [ images, emoji, math ],
+		rehypePlugins: [ katex ]
+	}
 })
 
-module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"]
-})
+const withCSS = require("@zeit/next-css")
+
+module.exports = withPlugins([
+	[ withCSS, {
+		// This settings required to import CSS files from packages.
+		// For example: import "katex/dist/katex.css"
+		cssLoaderOptions: {
+			url: false
+		}
+	}],
+
+	[ withMDX, {
+		pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"]
+	}]
+])
--- www/pages/about.md
+++ www/pages/about.md
@@ -1,6 +1,9 @@
+import "katex/dist/katex.css"
+import Nav from "../components/nav"
+
 # About
 
-Welcome to the test MD/MDX page.
+Welcome to the test MD/MDX page $2+2$.
 
 $$
 L = \frac{1}{2} \rho v^2 S C_L
