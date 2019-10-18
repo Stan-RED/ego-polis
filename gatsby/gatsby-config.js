5924fb9bb6c3f53183e140daef96f414675b82ef
stan <stanislav@yarmonov.pro>
2019-10-18 14:26:12 +0300

    +TypeScript support for Gatsby.js

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `TODO:Egopolis.org`,
+    title: `Egopolis`,
     description: `TODO:`,
     author: `TODO:@StanEgo`,
   },
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true,
+        allExtensions: true
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
