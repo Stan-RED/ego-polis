f84608b6bd278a7f5b0c9604bbc2e7a7a42981dd
stan <stanislav@yarmonov.pro>
2019-10-18 17:46:22 +0300

    *MDX integration for Gatsby.

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,42 +1,43 @@
 module.exports = {
   siteMetadata: {
-    title: `Egopolis`,
-    description: `TODO:`,
-    author: `TODO:@StanEgo`,
+    title: "Egopolis",
+    description: "TODO:",
+    author: "TODO:@StanEgo",
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
+        name: "images",
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
       },
     },
     {
-      resolve: `gatsby-plugin-typescript`,
+      resolve: "gatsby-plugin-typescript",
       options: {
         isTSX: true,
         allExtensions: true
       }
     },
-    `gatsby-plugin-postcss`
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-mdx"
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    // "gatsby-plugin-offline",
   ],
 }
