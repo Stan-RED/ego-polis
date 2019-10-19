67643b24a42d85e2bf99510e8880eab85c16174c
stan <stanislav@yarmonov.pro>
2019-10-19 21:36:03 +0300

    +DraftAlert for Gatsby articles.

--- gatsby/src/components/Layout.tsx
+++ gatsby/src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
+import { DraftAlert } from "../components/DraftAlert";
 import { GitHub } from "../components/icons";
 
 import "../styles/index.css";
@@ -16,14 +17,14 @@ const Layout = ({ children }: { children: ReactNode }) => {
             mdx {
                 id
                 frontmatter {
-                    date
-                    path
-                    state
                     title
+                    status
                 }
             }
         }
     `);
+
+    const { title, status } = data.mdx.frontmatter;
 
     return (
         <div className="flex flex-col min-h-screen">
@@ -43,7 +44,10 @@ const Layout = ({ children }: { children: ReactNode }) => {
             </header>
 
             <main className="flex-1 max-w-4xl mx-auto p-4 md:px-8 md:py-16 w-full">
-                <h1>{data.mdx.frontmatter.title}</h1>
+                {status === null || (status === "draft" && <DraftAlert />)}
+
+                <h1>{title}</h1>
+
                 {children}
             </main>
         </div>
