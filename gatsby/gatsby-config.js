38a86ab3df8c884d66eeec63407134e567f5b1c5
stan <stanislav@yarmonov.pro>
2019-10-19 19:23:49 +0300

    *Default layout for MDX configured.

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -36,9 +36,13 @@ module.exports = {
       }
     },
     "gatsby-plugin-postcss",
-    "gatsby-plugin-mdx"
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // "gatsby-plugin-offline",
-  ],
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/Layout.tsx")
+        }
+      }
+    }
+  ]
 }
