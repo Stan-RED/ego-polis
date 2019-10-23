b98682ddbae15bb86af01eb9b027a809705e37b7
stan <stanislav@yarmonov.pro>
2019-10-23 22:26:59 +0300

    *Link added to MDXProvider.

--- www/src/components/Layout.tsx
+++ www/src/components/Layout.tsx
@@ -1,13 +1,19 @@
 import React, { ReactNode } from "react";
 import { Link, useStaticQuery, graphql } from "gatsby";
+import { MDXProvider } from "@mdx-js/react";
 import { DraftAlert } from "../components/DraftAlert";
 import { GitHub } from "../components/icons";
+import { Link as MyLink } from "../components";
 import { SiteMetadata, LayoutContext, LayoutContextProps } from "../lib";
 
 import "../styles/index.css";
 
 type LayoutProps = LayoutContextProps & {
     children: ReactNode;
+};
+
+const mdxComponents = {
+    a: MyLink
 };
 
 const Layout = (props: LayoutProps) => {
@@ -27,28 +33,32 @@ const Layout = (props: LayoutProps) => {
 
     return (
         <LayoutContext.Provider value={props}>
-            <div className="flex flex-col min-h-screen">
-                <header className="border-black border-solid border-b-2 bg-gray-200">
-                    <div className="flex flex-wrap md:flex-no-wrap items-center justify-between max-w-4xl mx-auto p-4 md:p-6">
-                        <div className="flex items-center">
-                            <Link to="/" className="font-bold text-xl">
-                                {site.title}
-                            </Link>
+            <MDXProvider components={mdxComponents}>
+                <div className="flex flex-col min-h-screen">
+                    <header className="border-black border-solid border-b-2 bg-gray-200">
+                        <div className="flex flex-wrap md:flex-no-wrap items-center justify-between max-w-4xl mx-auto p-4 md:p-6">
+                            <div className="flex items-center">
+                                <Link to="/" className="font-bold text-xl">
+                                    {site.title}
+                                </Link>
+                            </div>
+                            <div>
+                                <a href={site.repository} className="font-bold">
+                                    <GitHub />
+                                </a>
+                            </div>
                         </div>
-                        <div>
-                            <a href={site.repository} className="font-bold">
-                                <GitHub />
-                            </a>
-                        </div>
-                    </div>
-                </header>
+                    </header>
 
-                <main className="flex-1 max-w-4xl mx-auto p-4 md:px-8 md:py-16 w-full">
-                    {meta && (meta.status === null || meta.status === "draft") && <DraftAlert />}
-                    {meta && <h1>{meta.title}</h1>}
-                    {props.children}
-                </main>
-            </div>
+                    <main className="flex-1 max-w-4xl mx-auto p-4 md:px-8 md:py-16 w-full">
+                        {meta && (meta.status === null || meta.status === "draft") && (
+                            <DraftAlert />
+                        )}
+                        {meta && <h1>{meta.title}</h1>}
+                        {props.children}
+                    </main>
+                </div>
+            </MDXProvider>
         </LayoutContext.Provider>
     );
 };
--- www/src/components/Link.tsx
+++ www/src/components/Link.tsx
@@ -4,11 +4,12 @@ import { LayoutContext, LayoutContextProps } from "../lib";
 
 export const Link = (props: any) => {
     const context = useContext<LayoutContextProps>(LayoutContext);
-    const { to, children, ...rest } = props;
-    const target = `/${context.pageContext.langKey}${to}`;
+    const { to, href, children, ...rest } = props;
+    const language = context.pageContext.langKey;
+    const target = to ? `/${language}${to}` : `/${language}${href}`;
     return (
         <GatsbyLink to={target} {...rest}>
-            {children} -> {target}
+            {children}
         </GatsbyLink>
     );
 };
