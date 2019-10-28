eee2f31ad8591f3b1ec11afcf973cba71b292a76
stan <stanislav@yarmonov.pro>
2019-10-28 22:32:40 +0200

    *Next.js MDX mapper experiment.

--- www-nextjs/next.config.js
+++ www-nextjs/next.config.js
@@ -1,6 +1,11 @@
-const withMDX = require("@next/mdx")({
+const fs = require("fs").promises;
+const babel = require("@babel/core");
+
+const configMDX = {
     extension: /\.mdx?$/
-});
+};
+const withMDX = require("@next/mdx")(configMDX);
+const mdx = require("@mdx-js/mdx");
 
 const config = {
     pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
@@ -9,6 +14,26 @@ const config = {
         defaultPathMap,
         { dev, dir, outDir, distDir, buildId }
     ) {
+        const path = `${dir}/pages/index.mdx`;
+        const content = await fs.readFile(path);
+        const transpile = async () => {
+            const jsx = await mdx(content);
+            return jsx;
+        }
+        await transpile().then(async js => {
+            const transformed = babel.transform(js, {
+                presets: [
+                    "@babel/preset-env",
+                    "@babel/preset-react"
+                ]
+            });
+
+            (function (exports) {
+                eval(transformed.code);
+                console.log(exports.meta);
+            })({});
+        });
+
         return {
             "/": { page: "/index", query: { test: "Stan" } },
             "/about": { page: "/index", query: { test: "world" } }
