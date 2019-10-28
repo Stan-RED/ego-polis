cbc4fd4444567d0b9e31daf00924a4142a481732
stan <stanislav@yarmonov.pro>
2019-10-28 22:07:53 +0200

    *Next.js TypeScript support.

---
+++ www-nextjs/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+
+    <div>
+      Hello world!
+    </div>
+  </div>
+)
+
+export default Home
