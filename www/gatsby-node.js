934dd3558abdbcb2127de7f0a765389069a4ed38
stan <stanislav@yarmonov.pro>
2019-11-24 18:28:39 +0200

    *Gatsby config fine-tuning.

--- www/gatsby-node.js
+++ www/gatsby-node.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const crypto = require("crypto");
 
 const MeshType = "Mesh"
@@ -25,60 +24,9 @@ exports.createSchemaCustomization = ({ actions }) => {
   createTypes(typeDefs)
 }
 
-const onCreateFile = ({ node, actions, createNodeId }) => {
+const onCreateFile = async ({ node, actions, createNodeId }) => {
   const { createNode, createParentChildLink } = actions;
   const id = createNodeId(`MESH:${node.id}`)
-
-  /*WORK:
-  {
-    id: '5adad60d-c47e-562b-a639-fe23144139a1',
-    children: [ '8436ea16-fb7a-59bb-ae3e-a5de14c66b09' ],
-    parent: null,
-    internal: {
-      contentDigest: '27154e83f8a278cc886ea29c39c05e1e',
-      type: 'File',
-      mediaType: 'text/mdx',
-      description: 'File "src\\pages\\sandbox\\egonomics\\index.ru.mdx"',
-      counter: 56,
-      owner: 'gatsby-source-filesystem',
-      fieldOwners: { slug: 'gatsby-plugin-i18n' }
-    },
-    sourceInstanceName: 'pages',
-    absolutePath: 'C:/Projects/ego-polis/www/src/pages/sandbox/egonomics/index.ru.mdx',
-    relativePath: 'sandbox/egonomics/index.ru.mdx',
-    extension: 'mdx',
-    size: 6772,
-    prettySize: '6.77 kB',
-    modifiedTime: '2019-11-29T13:19:46.868Z',
-    accessTime: '2019-11-30T08:07:52.987Z',
-    changeTime: '2019-11-29T13:19:46.868Z',
-    birthTime: '2019-10-24T05:22:07.085Z',
-    root: 'C:/',
-    dir: 'C:/Projects/ego-polis/www/src/pages/sandbox/egonomics',
-    base: 'index.ru.mdx',
-    ext: '.mdx',
-    name: 'index.ru',
-    relativeDirectory: 'sandbox/egonomics',
-    dev: 2686118167,
-    mode: 33206,
-    nlink: 1,
-    uid: 0,
-    gid: 0,
-    rdev: 0,
-    blksize: 4096,
-    ino: 3377699720572357,
-    blocks: 16,
-    atimeMs: 1575101272987.3286,
-    mtimeMs: 1575033586867.6792,
-    ctimeMs: 1575033586867.6792,
-    birthtimeMs: 1571894527084.6943,
-    atime: '2019-11-30T08:07:52.987Z',
-    mtime: '2019-11-29T13:19:46.868Z',
-    ctime: '2019-11-29T13:19:46.868Z',
-    birthtime: '2019-10-24T05:22:07.085Z',
-    fields: { slug: '/ru/sandbox/egonomics/' }
-  }
-  */
   const [title, language] = node.name.split(".");
   const slug = title === DefaultNodeSettings.indexfile ? "" : title;
 
@@ -101,8 +49,8 @@ const onCreateFile = ({ node, actions, createNodeId }) => {
     .update(JSON.stringify(meshNode))
     .digest("hex");
 
-  createNode(meshNode);
-  createParentChildLink({ parent: node, child: meshNode });
+  await createNode(meshNode);
+  await createParentChildLink({ parent: node, child: meshNode });
 }
 
 const onCreateMdx = ({ node, getNode, getNodesByType }) => {
@@ -124,15 +72,10 @@ const handlers = {
   "Mdx": onCreateMdx
 }
 
-exports.onCreateNode = (context) => {
+exports.onCreateNode = async (context) => {
   const handler = handlers[context.node.internal.type];
   handler && handler(context);
 }
-
-//WORK:
-// exports.onCreatePage = (context) => {
-//   console.log(context);
-// }
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
@@ -154,15 +97,13 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  edges.forEach(({ node }, index) => {
+  edges.forEach(async ({ node }, index) => {
     const page = {
       path: `${node.language}/${node.slug}`, //WORK:
       component: node.component,
       context: { mesh: node },
     };
 
-    console.log(page); //WORK:
-
-    createPage(page);
+    await createPage(page);
   })
 };
