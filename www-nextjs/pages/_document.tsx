a7dc62acbb819166ea98e2327f818efc40259542
stan <stanislav@yarmonov.pro>
2020-03-22 13:22:34 +0200

    *HTML layout.

---
+++ www-nextjs/pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+
+class MyDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+    }
+
+    render() {
+		return (
+			<Html>
+				<Head />
+				<body>
+					<main>
+						<Main />
+						<NextScript />
+					</main>
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
